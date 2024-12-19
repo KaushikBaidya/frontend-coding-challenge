60f466b96f48c3ede14219e78d56935ea66d5a8d
DESKTOP-Q5FMUEO\kaushik <kdiganto29@gmail.com>
2024-12-19 16:22:49 +0600

    update product page

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,37 +8,55 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const Products: React.FC = () => {
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
-  const {
-    currentPage,
-    totalPages,
-    paginatedItems: paginatedProducts,
-    handlePageChange,
-  } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
+	const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+	const {
+		currentPage,
+		totalPages,
+		paginatedItems: paginatedProducts,
+		handlePageChange,
+	} = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	useEffect(() => {
+		const productId = searchParams.get("product");
+		if (productId) {
+			const product = PRODUCTS_DATA.find((p) => p.id === productId);
+			if (product) {
+				setSelectedProduct(product);
+			}
+		}
+	}, [searchParams]);
 
-  const handleCloseModal = useCallback(() => {
-    setSelectedProduct(null);
-  }, []);
+	const handleOpenModal = useCallback(
+		(product: Product) => {
+			setSelectedProduct(product);
+			router.push(`/products?product=${product.id}`, undefined);
+		},
+		[router]
+	);
 
-  return (
-    <div>
-      <BackToHome />
-      <ProductList products={paginatedProducts} onOpenModal={handleOpenModal} />
-      <div className="h-4" />
-      <PaginationControls
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
-      {selectedProduct && (
-        <ProductModal product={selectedProduct} onClose={handleCloseModal} />
-      )}
-    </div>
-  );
+	const handleCloseModal = useCallback(() => {
+		setSelectedProduct(null);
+		router.push("/products", undefined);
+	}, [router]);
+
+	return (
+		<div>
+			<BackToHome />
+			<ProductList products={paginatedProducts} onOpenModal={handleOpenModal} />
+			<div className="h-4" />
+			<PaginationControls
+				currentPage={currentPage}
+				totalPages={totalPages}
+				onPageChange={handlePageChange}
+			/>
+			{selectedProduct && (
+				<ProductModal product={selectedProduct} onClose={handleCloseModal} />
+			)}
+		</div>
+	);
 };
